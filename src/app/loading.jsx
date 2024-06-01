226fdd25a3ba5820130d74cfe461036eac1f1628
ahmedfayez070 <ahmed070fayez@gmail.com>
2024-06-01 03:55:43 +0300

    style: change the loading page style ti a spinner loader instead of a paragraph

--- src/app/loading.jsx
+++ src/app/loading.jsx
@@ -1,7 +1,12 @@
+import styles from "./loading.module.css";
+
 const Loading = () => {
   return (
-    <div>Loading</div>
-  )
-}
+    <div className={styles.spinner}>
+      <div className={styles.doubleBounce1}></div>
+      <div className={styles.doubleBounce2}></div>
+    </div>
+  );
+};
 
-export default Loading
+export default Loading;
